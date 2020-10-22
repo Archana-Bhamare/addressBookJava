4bca9df1abca02c4c57f93f93e5b6ab216035b1f
Archana-Bhamare <archusonar2@gmail.com>
2020-10-22 19:44:33 +0530

    Added : User Input Details

--- src/addressBook/AddressBookJava.java
+++ src/addressBook/AddressBookJava.java
@@ -1,8 +1,11 @@
 package addressBook;
 
 public class AddressBookJava {
-
 	public static void main(String[] args) {
-		System.out.println("Welcome To Address Book!!!");
+		String fname = null, lname = null, mnum = null, email = null, city = null, state = null, zip = null;
+		String name = "^[A-Z]{1}[a-z]{3,}$";
+		String mob = "^[91]{2}[ ]?[0-9]{10}$";
+		String mail = "^[a-zA-Z0-9]+([.+-_][a-z-A-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,3}([.][a-z]{2})*$";
+		String zipcode = "^[0-9]{3}[ ]?[0-9]{3}$";
 	}
 }
