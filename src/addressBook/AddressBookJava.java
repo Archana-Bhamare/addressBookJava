410b63cfc0665a1d89fbca29c6b5e9cdd6a1750d
Archana-Bhamare <archusonar2@gmail.com>
2020-10-23 16:14:02 +0530

    Modify Code : Add file

--- src/addressBook/AddressBookJava.java
+++ src/addressBook/AddressBookJava.java
@@ -1,14 +1,15 @@
 package addressBook;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.*;;
+
 public class AddressBookJava {
 	ArrayList<String> persons = new ArrayList<>();
 	String addressbook;
@@ -18,7 +19,7 @@ public class AddressBookJava {
 	String mail = "^[a-zA-Z0-9]+([.+-_][a-z-A-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,3}([.][a-z]{2})*$";
 	String zipcode = "^[0-9]{3}[ ]?[0-9]{3}$";
 
-	public  void userInput() {
+	public void userInput() {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the First Name : ");
 		fname = s.next();
@@ -40,7 +41,8 @@ public class AddressBookJava {
 			System.out.println("Please enter the name in valid Format, First letter should be Capital");
 			lname = s.next();
 		}
-	/*	System.out.println("Enter the Mobile Number : ");
+
+		System.out.println("Enter the Mobile Number : ");
 		mnum = s.nextInt();
 		Pattern pattern2 = Pattern.compile(name);
 		Matcher matcher2 = pattern2.matcher(mnum);
@@ -49,7 +51,8 @@ public class AddressBookJava {
 		} else {
 			System.out.println("Please enter the valid mobile number");
 			mnum = s.nextInt();
-		}*/
+		}
+
 		System.out.println("Enter the City  : ");
 		city = s.next();
 		Pattern pattern3 = Pattern.compile(name);
@@ -70,7 +73,8 @@ public class AddressBookJava {
 			System.out.println("Please enter the state name in proper format");
 			state = s.next();
 		}
-		/*System.out.println("Enter the Zip : ");
+
+		System.out.println("Enter the Zip : ");
 		zip = s.nextInt();
 		Pattern pattern5 = Pattern.compile(name);
 		Matcher matcher5 = pattern5.matcher(zip);
@@ -79,49 +83,57 @@ public class AddressBookJava {
 		} else {
 			System.out.println("Invalid zip code");
 			zip = s.nextInt();
-		}*/
+		}
+
 	}
 
-	public void insertUser() {
+	public void insertUser() throws IOException {
 		userInput();
-		String p=new String(fname);
-		String p1=new String(lname);
-		String p2=new String(city);
-		String p3=new String(state);
+		String p = new String(fname);
+		String p1 = new String(lname);
+		String p2 = new String(city);
+		String p3 = new String(state);
 		persons.add(p);
 		persons.add(p1);
 		persons.add(p2);
 		persons.add(p3);
 		System.out.println(persons);
-		try {
-		ArrayList<String> persons = new ArrayList<>(Files.readAllLines(Paths.get(addressbook)));
-		/*try (BufferedReader reader=new BufferedReader(new FileReader(addressbook))){
-			while (reader.ready()){
-				persons.add(reader.readLine());
-			}*/
-		}catch (IOException e) {
-			// TODO: handle exception
-		} 
-		catch (java.lang.NullPointerException e) {
-			// TODO: handle exception
-		}
-}
-	
-	public static void main(String[] args) {
+		File f1 = new File("G:/addressbook.txt");
+		f1.createNewFile();
+		FileWriter fw = new FileWriter("G:/addressbook.txt");
+		fw.write("\nFirst Name : " + fname + "\nLast Name : " + lname + "\nCity : " + city + "\nState : " + state);
+		fw.close();
+	}
+
+	public static void main(String[] args) throws IOException {
 		AddressBookJava add = new AddressBookJava();
-		add.insertUser();
-		/*
-		 * while (true) { Scanner s = new Scanner(System.in);
-		 * System.out.println(
-		 * "1.Create AddressBook\n2.Display AddressBook\n3.Add User Information\n4.Modify Details\n5.Delete user Record\n6.Search By Name\n7.Search By Zip\n8.Exit"
-		 * ); int choice = s.nextInt();
-		 * 
-		 * switch (choice) { case 1: add.createAddressBook(); break; case 2:
-		 * add.displayAddressBook(); break; case 3: add.insertUser(); break;
-		 * case 4: add.modify(); break; case 5: add.deleteRecord(); break; case
-		 * 6: add.searchByName(); break; case 7: add.searchByZip(); break; case
-		 * 8: System.exit(0); default: System.out.println("Invalid Choice");
-		 * break; }
-		 */
+		while (true) {
+			Scanner s = new Scanner(System.in);
+			System.out.println(
+					"1.Add User Information\n2.Display AddressBook\n3.Modify Details\n4.Delete user Record\n5.Search Record\n6.Exit");
+			int choice = s.nextInt();
+
+			switch (choice) {
+			case 1:
+				add.insertUser();
+				break;
+			case 2:
+				add.displayAddressBook();
+				break;
+			case 3:
+				add.modify();
+				break;
+			case 4:
+				add.deleteRecord();
+				break;
+			case 5:
+				add.searchRecord();
+			case 6:
+				System.exit(0);
+			default:
+				System.out.println("Invalid Choice");
+				break;
+			}
+		}
 	}
 }
